8b0ca1b6f31551102da7dc5fbcd0c88ece81c965
lsca <lsca@cin.ufpe.br>
2017-08-30 15:11:29 -0300

    Removing messageController

--- uMQTTPublish.java
+++ uMQTTPublish.java
@@ -5,7 +5,6 @@ import android.support.annotation.IntDef;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import re.usto.message.controller.MessageController;
 import timber.log.Timber;
 
 /**
